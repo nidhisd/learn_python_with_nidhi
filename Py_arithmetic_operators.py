439076c51f6e6f1b6c8749ce6bb9de0290951aaf
Moiz <moizjv@gmail.com>
2018-11-13 12:59:59 -0800

    first commit

---
+++ Py_arithmetic_operators.py
@@ -0,0 +1,7 @@
+def test_arithmetic_operators():
+    assert 5 + 3 == 8, "High Five!"
+    assert 2 - 3 == -1, "Minus One!"
+    assert 5 // 3 == 1, "High 1 !"
+    assert isinstance(5 * 3, int)
+
+test_arithmetic_operators()
