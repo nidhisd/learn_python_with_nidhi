1dd4d424edc85dbca0065d6254de11ad420f8dd9
nidhisd <nidhidhammani@gmail.com>
2018-11-13 16:21:55 -0800

    Hands-on for comparison operators

---
+++ comparison_operators.py
@@ -0,0 +1,28 @@
+""" Comparison operators
+
+Comparison operators are used to compare two values
+"""
+
+def comparison_operators():
+
+    #Equal-to
+
+    number = 9
+    assert number == 9
+
+    #Not-Equal-to
+    number = 4
+    assert number != 2
+
+    #Less-than-equal-to
+    number = 3
+    assert number <= 3
+    assert number < 4
+    assert number < 5
+
+    #greater-than-equal-to
+    assert number > 2
+    assert number >= 3
+    assert number > 1
+    
+comparison_operators()
