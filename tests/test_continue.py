6a328b5ae5c90054ec26909f94cf5b0d0796f12a
nidhisd <nidhidhammani@gmail.com>
2018-12-14 13:16:16 -0800

    Added some theory on Continue

--- tests/test_continue.py
+++ tests/test_continue.py
@@ -1,3 +1,10 @@
+
+"""CONTINUE statement
+
+@see: https://docs.python.org/3/tutorial/controlflow.html
+
+    The continue statement is borrowed from C, continues with the next iteration of the loop.
+"""
 def test_continue():
 
     even_numbers = []
@@ -6,6 +13,7 @@ def test_continue():
     for number in range(0, 10):
         if number % 2 == 0:
             even_numbers.append(number)
+            #Stops current loop iteration and go to the next iteration immediately.
             continue
         odd_numbers.append(number)
     
