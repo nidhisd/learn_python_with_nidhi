a2ae49faec8f3ea562d8153cf88c32a26b205f9a
nidhisd <nidhidhammani@gmail.com>
2018-12-17 15:56:12 -0800

    Functions Hands-on

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -16,5 +16,31 @@ def test_fibonacci():
     fibonacci_function_clone = fibonacci_functions
     assert fibonacci_function_clone(10) == [0, 1, 1, 2, 3, 5, 8]
 
+def test_functions_definitions():
+
+    def greet(name):
+        return 'Hello ' + name
+
+    greet_someone = greet
+
+    assert greet_someone('Gary') == "Hello Gary"
+
+    def greet_again(name):
+        def get_greet_message():
+            return "Hi! "
+        return get_greet_message() + name
+    
+    assert greet_again('Henry') == "Hi! Henry"
+
+    #Functions can be passed as a parameter of other function.
+
+    def person_name(func):
+        other_person = 'Jack'
+        return func(other_person)
+    
+    assert person_name(greet_again) == "Hi! Jack"
+
+    
+
 
 
