e15747004845bda59f16a86d2b6f2832c9a41ac9
nidhisd <nidhidhammani@gmail.com>
2018-12-20 11:08:55 -0800

    Fixing Non-local declaration errors

--- tests/test_function_scope.py
+++ tests/test_function_scope.py
@@ -4,20 +4,20 @@ def test_function_scope():
     variable_scope = "Global"
 
     def test_local():
-        variable_scope = "Local"
         nonlocal variable_scope
         return variable_scope
-    
+
     def test_non_local():
-        nonlocal variable_scope 
+        nonlocal variable_scope
         variable_scope = "Non Local"
         return variable_scope
-    
+
     def test_global():
-        global variable_scope 
+        global variable_scope
         variable_scope = "Global"
         return variable_scope
-    
+
+
     print("Current scope : " + variable_scope)
     test_local()
     print("Current scope : " + variable_scope)
